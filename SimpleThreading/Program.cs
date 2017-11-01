7e90ce6c77cd2dea68b760c9ca2d890d1af62cb2
Ravi Dirckze <r_dirckze@yahoo.com>
2017-11-01 00:32:56 -0700

    Simple Task program added to solution

---
+++ SimpleThreading/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SimpleThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program is starting ...");
+            SimpleTask simpleTask = new SimpleTask();
+            Thread.Sleep(100);
+            simpleTask.Command = SimpleTask.CommandType.Terminate;
+            Console.WriteLine("Program is done ... Press any key to terminate.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimpleThreading/SimpleTask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleThreading
+{
+    public class SimpleTask
+    {
+
+        public enum CommandType { Initial, Run, Complete, Terminate };
+
+        public CommandType Command { get; set; }
+
+        public Thread  TaskRunner = null;
+
+        public SimpleTask()
+        {
+            this.Command = CommandType.Initial;
+
+            TaskRunner = new Thread(DoTask);
+            TaskRunner.Start();
+        }
+
+        public void DoTask()
+        {
+            while (this.Command != CommandType.Complete && this.Command != CommandType.Terminate)
+            {
+                Console.WriteLine("DoTask is doing something ...");
+                TaskRunner.Join(10);
+            }
+
+            Console.WriteLine("DoTask command is: {0}", Command.ToString());
+        }
+
+    }
+}
